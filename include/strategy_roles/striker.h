14992f5baa60d81f80b439d07f401a0abb21cd6a
emamaker <emamaker0@gmail.com>
2022-07-04 15:31:01 +0200

    striker: adapt striker to new ball reading

--- include/strategy_roles/striker.h
+++ include/strategy_roles/striker.h
@@ -1,32 +1,32 @@
 #pragma once
 
-#include "sensors/data_source_camera_vshapedmirror.h"
+#include "behaviour_control/complementary_filter.h"
+#include "motors_movement/drivecontroller.h"
 #include "sensors/sensors.h"
 #include "strategy_roles/game.h"
-#include "motors_movement/drivecontroller.h"
 
 #define STRIKER_ATTACK_DISTANCE 110
 #define STRIKER_TILT_STOP_DISTANCE 140
-#define STRIKER_PLUSANG 50
+#define STRIKER_PLUSANG 35
 #define STRIKER_PLUSANG_VISIONCONE 7
 #define STRIKER_VEL MAX_VEL
 
-#define STRIKER_MOUTH_SX 345
-#define STRIKER_MOUTH_DX 15
+class Striker : public Game
+{
 
-class Striker : public Game{
+public:
+    Striker();
+    Striker(LineSystem *ls, PositionSystem *ps);
 
-    public:
-        Striker();
-        Striker(LineSystem* ls, PositionSystem* ps);
+private:
+    void realPlay() override;
+    void init() override;
+    void striker();
+    int tilt();
+    float ballTilt();
 
-    private:
-        void realPlay() override;
-        void init() override;
-        void striker();
-        int tilt();
-        float ballTilt();
+    int atk_speed, atk_direction = 0, atk_tilt, stato = 0, plusang_flag = 0;
+    bool flag = false;
 
-        int atk_speed, atk_direction = 0, atk_tilt, stato = 0, plusang_flag = 0;
-        bool flag = false;
+    ComplementaryFilter *ball_filter;
 };
