375f812d793b0ef4c2f20363a8a2c15f1dfe6f44
emamaker <emamaker0@gmail.com>
2022-07-08 10:33:31 +0200

    striker: revise values+go straight if ball distant

--- src/strategy_roles/striker.cpp
+++ src/strategy_roles/striker.cpp
@@ -40,55 +40,51 @@ void Striker::striker()
 
     if (CURRENT_DATA_READ.lineActive != 0) flag = false;
 
-    int dir = 0, ball_deg = ball_filter->calculate(CURRENT_DATA_READ.ballAngle), plusang = 0; //, plusang = STRIKER_PLUSANG + 35 * (CURRENT_DATA_READ.ballAngle <= 90);
-                                                                                              // if (plusang >= 80) plusang = 80;
+    int dir = 0, ball_deg = ball_filter->calculate(CURRENT_DATA_READ.ballAngle);
 
-    // if (CURRENT_DATA_READ.ballDistance >= 120)
-    // {
-    //     drive->prepareDrive(ball_deg > 180 ? ball_deg * 0.96 : ball_deg * 1.04, STRIKER_VEL, 0);
-    // }
-    // else
-    // {
-    // seguo palla
-
-    if (ball_deg >= 345 || ball_deg <= 25)
+    if (CURRENT_DATA_READ.ballDistance >= 120)
     {
-        dir = 0;
-        flag = false;
+        drive->prepareDrive(ball_deg > 180 ? ball_deg * 0.96 : ball_deg * 1.04, STRIKER_VEL, 0);
     }
     else
     {
-        double ball_x = sin(radians(ball_deg)) * CURRENT_DATA_READ.ballDistance;
-        double ball_y = cos(radians(ball_deg)) * CURRENT_DATA_READ.ballDistance;
-        double ball_y_new, ball_x_new;
-        if (ball_deg <= 100 || ball_deg >= 260)
+        if (ball_deg >= 345 || ball_deg <= 15 || flag)
         {
-            ball_y_new = ball_y - 100;
-            ball_x_new = 0;
+            dir = 0;
+            flag = true;
+            if (ball_deg <= 330 && ball_deg >= 30) flag = false;
         }
         else
         {
-            ball_y_new = ball_y - 100;
-            if (ball_deg >= 180)
-                ball_x_new = ball_x - 100;
+            flag = false;
+            double ball_x = sin(radians(ball_deg)) * CURRENT_DATA_READ.ballDistance;
+            double ball_y = cos(radians(ball_deg)) * CURRENT_DATA_READ.ballDistance;
+            double ball_y_new, ball_x_new;
+            if (ball_deg <= 100 || ball_deg >= 260)
+            {
+                ball_y_new = ball_y - 70;
+                ball_x_new = ball_x;
+            }
             else
-                ball_x_new = ball_x + 100;
+            {
+                flag = false;
+                ball_y_new = ball_y - 70;
+                if (ball_deg >= 180)
+                    ball_x_new = ball_x + 100;
+                else
+                    ball_x_new = ball_x - 100;
+            }
+
+            int new_angle = 90 - (int)degrees(atan2(ball_y_new, ball_x_new));
+            if (new_angle < 0) new_angle += 360;
+
+            dir = new_angle;
+            // DEBUG.println(String(ball_deg) + ", " + String(new_angle));
         }
 
-        int new_angle = 90 - (int)degrees(atan2(ball_y_new, ball_x));
-        if (new_angle < 0) new_angle += 360;
-
-        dir = new_angle;
+        dir = (dir + 360) % 360;
+        drive->prepareDrive(dir, STRIKER_VEL, tilt_filter->calculate(CURRENT_DATA_READ.atkGAngle_fix));
     }
-
-    // if (CURRENT_DATA_READ.atkSeen) atk_tilt = CURRENT_DATA_READ.angleAtkFix;
-    dir = (dir + 360) % 360;
-    drive->prepareDrive(dir, STRIKER_VEL, constrain(tilt_filter->calculate(CURRENT_DATA_READ.angleAtkFix), -25, 25));
-    // DEBUG.println("Ball: " + String(ball_deg) + "/" + String(CURRENT_DATA_READ.ballDistance) + " Direction: " + String(dir) + "(" + String(ball_deg) + "+" + String(plusang) + ")");
-
-    // if(ball->isInFront() && roller->roller_armed) roller->speed(ROLLER_DEFAULT_SPEED);
-    // else roller->speed(roller->MIN);
-    // }
 }
 
 int Striker::tilt()
